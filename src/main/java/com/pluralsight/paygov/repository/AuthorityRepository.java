f10c06351f7bb0f54e1976dc343006dd21844f5c
samuel <samuel6tesfay@gmail.com>
2022-06-08 08:57:14 +0300

    Initial version of PayGov generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/pluralsight/paygov/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.pluralsight.paygov.repository;
+
+import com.pluralsight.paygov.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/pluralsight/paygov/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.pluralsight.paygov.service.dto;
